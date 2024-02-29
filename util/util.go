1dfed0afe555b79675cf674977c00847acb2e973
stellaratlas8 <ajmalinao1@gmail.com>
2024-02-29 16:58:13 +0800

    clean util

--- util/util.go
+++ util/util.go
@@ -1,1 +1,22 @@
 package util
+
+// import "reflect"
+
+// func ContainsRequiredFields(obj *interface{}) bool {
+// 	// va
+// 	// val := reflect.ValueOf(obj).NumField()
+// 	// for i := 0; i<val.NumField(); i++ {
+// 	// 	field := val.Field(i)
+// 	// 	field.Name
+// 	// }
+// 	return false
+// }
+
+// func GetField(obj *reflect.StructField, field string) (reflect.StructField, bool) {
+// 	return reflect.TypeOf(obj).FieldByName(field)
+// }
+
+// func GetTag(field *reflect.StructField, tag string) (string, bool) {
+// 	result, ok := field.Tag.Lookup(tag)
+// 	return string(result), ok
+// }
